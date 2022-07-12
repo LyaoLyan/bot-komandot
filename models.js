acc84ef56dbd8824466a94ab865979866d6ed727
ismagilov-anton <ismagilov-anton@bauart.pro>
2022-07-12 22:09:10 +0700

    Added image property to user

--- models.js
+++ models.js
@@ -1,5 +1,6 @@
-const sequelize = require('./db');
-const { DataTypes } = require('sequelize');
+const sequelize = require('./db')
+const { DataTypes } = require('sequelize')
+
 const User = sequelize.define('user', {
   id: { type: DataTypes.INTEGER, primaryKey: true, unique: true, autoIncrement: true },
   admin: {type: DataTypes.BOOLEAN, defaultValue: false},
@@ -8,10 +9,10 @@ const User = sequelize.define('user', {
   paycheck: { type: DataTypes.STRING, unique: true },
   name: { type: DataTypes.STRING },
   phone: { type: DataTypes.STRING, unique: true },
-
+  image: { type: DataTypes.STRING(1234) }
 },
   {
     tableName: 'users',
   });
 
-module.exports = User;
+module.exports = User
